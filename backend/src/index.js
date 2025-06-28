7a841f04ca1f28bc864146e9a0cacfd177ac1348
mePradipta30 <pradiptabanerjee02@gmail.com>
2025-06-28 10:59:33 +0530

    change name to whispr

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import { server, app } from "./lib/socket.js";
 dotenv.config();
 
 // Port and Directory Setup
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 const __dirname = path.resolve();
 
 // Middlewares
